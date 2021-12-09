71911eaeaffdc21d01a0b6ecf059d3dde960182c
xianzhi <yuanxianzhipku@163.com>
2021-12-09 15:42:07 +0800

    pgtbl partially done

--- kernel/exec.c
+++ kernel/exec.c
@@ -115,7 +115,11 @@ exec(char *path, char **argv)
   p->trapframe->epc = elf.entry;  // initial program counter = main
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
-
+  //pgtbl-lab
+  if((p->pid) == 1) {
+	  printf("page table %p\n",p->pagetable);	  
+	  vmprint(p->pagetable,0);
+  }
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
  bad:
--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,20 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void 
+vmprint(pagetable_t pagetable, int L)
+{
+	// printf("page table %p\n",pagetable);// the address of the first level pgtbl;
+	for(int i = 0; i < 512; ++i) {
+		pte_t pte = pagetable[i];
+	      	if(pte & PTE_V) {
+			uint64 child = PTE2PA(pte);
+			if(L == 1) { printf(".. "); }
+			if(L == 2) { printf(".. .. "); }
+			printf("..%d: pte %p pa %p\n",i,pte,child);
+			
+			if(L < 2) { vmprint((pagetable_t) child, L+1 );}
+		}
+	}
+}
